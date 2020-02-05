38861cc71793250f92f1d67db2edcf0af8988bea
Praneeth RK <praneethrk17@gmail.com>
2020-02-06 00:48:12 +0530

    graph updated

--- src/app/analytics/statement1/statement1.component.ts
+++ src/app/analytics/statement1/statement1.component.ts
@@ -23,10 +23,10 @@ export class Statement1Component implements OnInit {
   user_info;
   showSpinner = false;
   dept;
-  event:any;
-  email:any ="";
-  offers:any[] = [];
-  placement:boolean=false;
+  event: any;
+  email: any = "";
+  offers: any[] = [];
+  placement: boolean = false;
   ueScore;
   constructor(private analyticsService: AnalyticsService, private authService: AuthService) { }
 
@@ -44,53 +44,44 @@ export class Statement1Component implements OnInit {
 
   get_term_numbers() {
     this.analyticsService.get_term_details().subscribe(res => {
-      this.termnumbers = res['term_numbers']
+      this.termnumbers = res['term_numbers'];
     }
     )
   }
 
-  get_dept(){
-    this.analyticsService.get_user_dept(this.user_info['email']).subscribe(res=>{
+  get_dept() {
+    this.analyticsService.get_user_dept(this.user_info['user']).subscribe(res => {
       this.dept = res["res"];
-      console.log(this.dept)
     })
   }
   searchbutton() {
-    if(!this.placement){
+    if (!this.placement) {
 
-      this.analyticsService.get_placement_offers(this.academicYears, this.user_info['usn']).subscribe(res=>{
+      this.analyticsService.get_placement_offers(this.academicYears, this.user_info['usn']).subscribe(res => {
         let result = res["res"];
-        for(let res of result)
-        {
-          this.offers.push([res['companyName'],res['salary']])
+        for (let res of result) {
+          this.offers.push([res['companyName'], res['salary']])
         }
         console.log(this.offers);
       })
       this.placement = true
     }
 
-    this.analyticsService.get_uemarks(this.academicYears,"IS",this.user_info['usn'],"6").subscribe(res => {
+    this.analyticsService.get_uemarks(this.selectedyear, this.dept, this.user_info['usn'], this.terms).subscribe(res => {
       this.ueScore = res['res'];
       this.score(this.ueScore);
     })
-  
+
   }
 
-  score(data){
+  score(data) {
     let dataTable = []
     dataTable.push([
       "courseName",
-      "ueScore %",
-      // "Attendance %", { type: 'string', role: 'tooltip' }
+      "ueScore %"
     ]);
-    // for (let i = 0; i < data.length; i += 1) {
-    //   dataTable.push([data[i]['courseName'],
-    //   data[i]['avg_ia_score'], "IA % : " + data[i]['avg_ia_score'] + "\n" +
-    //   "Attendance % : " + data[i]['attendance_per'], data[i]['attendance_per'], "IA % : " + data[i]['avg_ia_score'] + "\n" +
-    //   "Attendance % : " + data[i]['attendance_per']])
-    // }
-    for(let d of data){
-      dataTable.push([d['courseName'],d['ueScore']]);
+    for (let d of data) {
+      dataTable.push([d['courseName'], d['ueScore']]);
 
     }
     if (dataTable.length > 1) {
@@ -102,19 +93,38 @@ export class Statement1Component implements OnInit {
       this.error_flag = true
       this.error_message = "Data doesnot exist for the entered criteria"
     }
+
   }
   graph_data(data) {
     this.showSpinner = false
-    this.title = 'Course-wise Attendance %',
+    this.title = 'Course-wise Performence %',
       this.firstLevelChart = {
         chartType: "ColumnChart",
         dataTable: data,
         options: {
-          height:800,
-         
+          bar: { groupWidth: "13%" },
+          height: 400,
+          vAxis: {
+            title: "Performance %",
+          },
+          hAxis: {
+            title: "Courses",
+          },
+          chartArea: {
+            left: 80,
+            right: 80,
+            top: 100,
+          },
+          legend: {
+            title:"x axis",
+            position: "top",
+            alignment: "end"
+          },
+          seriesType: "bars",
+          colors: ["#669999"],
+          fontName: "Times New Roman",
+          fontSize: 13,
         }
-
-
+      }
   }
-}
 }
